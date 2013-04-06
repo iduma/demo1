20d18563665a8539eb17b7f9e722bd0f9f1c4dcc
iduma <shijiahui@gmail.com>
2013-04-06 23:14:57 +0800

    change db uri

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'#os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 class User(db.Model):
